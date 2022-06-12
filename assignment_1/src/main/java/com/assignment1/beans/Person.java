ce9b754f2880615a07e444e9be0d45461eb64d14
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-06-12 08:04:24 -0700

    Update Person.java

--- assignment_1/src/main/java/com/assignment1/beans/Person.java
+++ assignment_1/src/main/java/com/assignment1/beans/Person.java
@@ -3,12 +3,11 @@ package com.assignment1.beans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component(value = "personBean")
-public class Person {
+@Component(value = "personBean")      //we can customize bean value to "personbean", or by default IoC container will maintain this bean with class name, Person.
     private String name = "Lucy";
     private final Vehicle vehicle;
 
-    @Autowired
+    @Autowired     // inject dependency by constructor
     public Person(Vehicle vehicle){
         this.vehicle = vehicle;
     }
