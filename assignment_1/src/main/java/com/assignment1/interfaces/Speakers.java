1ad09fb63cf153b7d636e018328448a2e9704793
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-06-12 08:21:30 -0700

    Update Speakers.java

--- assignment_1/src/main/java/com/assignment1/interfaces/Speakers.java
+++ assignment_1/src/main/java/com/assignment1/interfaces/Speakers.java
@@ -1,5 +1,6 @@
 package com.assignment1.interfaces;
 
+// we cannot create beans from interfaces. we can only create beans from java classes, so we don't use @ here
 public interface Speakers {
     public String makeSound();
 }
