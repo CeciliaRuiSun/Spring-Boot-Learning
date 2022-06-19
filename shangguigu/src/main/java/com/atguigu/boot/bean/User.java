90672d84d027d0535d6b1685ed3fb8f2c0cc9a79
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2022-06-18 22:33:41 -0700

    Add files via upload

---
+++ shangguigu/src/main/java/com/atguigu/boot/bean/User.java
@@ -0,0 +1,22 @@
+package com.atguigu.boot.bean;
+
+import lombok.*;
+
+@NoArgsConstructor
+// @AllArgsConstructor
+@Data
+@ToString
+@EqualsAndHashCode
+public class User {
+    private String name;
+    private Integer age;
+    private Pet pet;
+
+
+    public User(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
